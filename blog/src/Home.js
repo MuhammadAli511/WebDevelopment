02819942344728d9d79758e9acf0792c3d087743
MuhammadAli511 <mohammadali417@outlook.com>
2022-08-27 12:30:15 +0500

    Tutorial 7

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,9 +1,23 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello", e)
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("Hello " + name)
+        console.log(e)
+    }
+
     return ( 
     <div className="home">
         <h2>Home Page</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => {
+            handleClickAgain('mario',e)
+        }}>Click me again</button>
     </div>
-     );
+    );
 }
  
 export default Home;
